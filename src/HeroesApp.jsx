3f8ec2b5824a2f052a5537743924ebc38fd84a45
Agustindardanelli-stack <agusdardanelli16@gmail.com>
2023-09-13 12:08:20 -0300

    Push Heroes-Spa

---
+++ src/HeroesApp.jsx
@@ -0,0 +1,15 @@
+import { AppRouter } from './router/AppRouter';
+import { AuthProvider } from './auth';
+
+export const HeroesApp = () => {
+  return (
+    <AuthProvider>
+
+      <AppRouter />
+
+
+    </AuthProvider>
+        
+    
+  )
+}
